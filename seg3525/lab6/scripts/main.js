2cfa29427a46a70300ef6b375a35596a86a06146
Oren Scheer <oren.rs723@gmail.com>
2021-06-27 18:02:19 -0400

    Finish booking form

--- seg3525/lab6/scripts/main.js
+++ seg3525/lab6/scripts/main.js
@@ -28,7 +28,19 @@ $(document).ready(() => {
         "minTime": "8:00am",
         "maxTime": "5:00pm",
         "listWidth": 1,
-    });
+    }).on("timeFormatError", function () {
+        addError(false, "timepicker")
+    }).on("timeRangeError", function () {
+        addError(false, "timepicker");
+    }).on("changeTime", function () {
+        removeError(false, "timepicker");
+    }).change(function () {
+        if ($(this).val()) {
+            removeError(false, "timepicker");
+        } else {
+            addError(false, "timepicker");
+        }
+    })
 
     $("#bookModal").on("hidden.bs.modal", () => {
         $("#book-form").trigger("reset");
@@ -43,40 +55,122 @@ $(document).ready(() => {
     $("#book-form-submit").click(function () {
         $("#confirmationModalLabel").text(`Thanks for booking, ${$("#name").val().split(" ")[0]}!`);
         $("#confirmation-text").text(`Your appointment has been booked for ${$("#datepicker").val()} at ${$("#timepicker").val()}.`);
-        $("#confirmation-email").text(`We have sent a confirmation email to ${$("#email").val()}.`);
+        $("#confirmation-email").text(`We have sent a confirmation email to ${$("#book-form .email").val()}.`);
     });
 
     $("#book-form").change(function () {
         if ($("#book-form input").hasClass("form-control-invalid")) {
             $("#book-form-error").show().text("You have errors. Please correct them before submitting.");
-            $("#book-form-submit").prop("disabled", true);
         } else {
             $("#book-form-error").hide();
-            $("#book-form-submit").prop("disabled", false);
         }
     })
         .submit(function () {
-            if ($("#book-form input").hasClass("form-control-invalid")) {
+            if ($("#book-form .required-simple").val() == "") {
+                $("#book-form-error").show().text("You have errors. Please correct them before submitting.");
+                if ($("#book-form #name").val() === "") {
+                    addError(false, "name");
+                }
+                if ($("#book-form #datepicker").val() === "") {
+                    addError(false, "datepicker");
+                }
+                if ($("#book-form #timepicker").val() === "") {
+                    addError(false, "timepicker");
+                }
+                if ($("#book-form .email").val() === "") {
+                    addError(true, "email");
+                }
+                if ($("#book-form .phone").val() === "") {
+                    addError(true, "phone");
+                }
+                if ($("#book-form #credit-card").val() === "") {
+                    addError(false, "credit-card");
+                }
+                return;
+            } else if ($("#book-form input").hasClass("form-control-invalid")) {
                 return;
             }
             $("#bookModal").modal("hide");
             $("#confirmationModal").modal("toggle");
         });
 
+    $("#name").change(function () {
+        if ($(this).val()) {
+            removeError(false, "name");
+        } else {
+            addError(false, "name");
+        }
+    });
+
+    $("#datepicker").change(function () {
+        if ($(this).val()) {
+            removeError(false, "datepicker");
+        } else {
+            addError(false, "datepicker");
+        }
+    })
+
     $(".phone").change(function () {
         let reg = /^\(\d{3}\)-\d{3}-\d{4}$/
         if (!reg.test($(this).val())) {
-            $(this).addClass("form-control-invalid");
-            $("#phone-invalid").show();
+            addError(true, "phone");
         } else {
-            $(this).removeClass("form-control-invalid");
-            $("#phone-invalid").hide();
+            removeError(true, "phone");
         }
     })
+
+    $(".email").change(function () {
+        let reg = /^.+@.+\..+$/
+        if (!reg.test($(this).val())) {
+            addError(true, "email");
+        } else {
+            removeError(true, "email");
+        }
+    })
+
+    $("#credit-card").change(function () {
+        let reg = /^\d{4} \d{4} \d{4} \d{4}$/
+        if (!reg.test($(this).val())) {
+            addError(false, "credit-card");
+        } else {
+            removeError(false, "credit-card");
+        }
+    })
+
+    $("#credit-card-info").tooltip().hover(function () {
+        $(this).removeClass("bi-question-circle");
+        $(this).addClass("bi-question-circle-fill");
+    }, function () {
+        $(this).removeClass("bi-question-circle-fill");
+        $(this).addClass("bi-question-circle");
+    });
+
+    function addError(isClass, name) {
+        let sel;
+        if (isClass) {
+            sel = `#book-form .${name}`;
+        } else {
+            sel = `#book-form #${name}`;
+        }
+        $(sel).addClass("form-control-invalid");
+        $(`#${name}-invalid`).show();
+    }
+
+    function removeError(isClass, name) {
+        let sel;
+        if (isClass) {
+            sel = `#book-form .${name}`;
+        } else {
+            sel = `#book-form #${name}`;
+        }
+        $(sel).removeClass("form-control-invalid");
+        $(`#${name}-invalid`).hide();
+    }
 
     function removeInvalidFeedback() {
         $("#book-form .invalid-feedback").hide();
         $("#book-form input").removeClass("form-control-invalid");
+        $("#book-form-error").hide();
     }
 });
 
